18cc6d53881bd14ace4d007471755063f43da7bf
Bwana Paul <p.ndungu.kara@gmail.com>
2023-12-08 07:29:37 +0300

    Created the project and added the code

---
+++ Coin Flip Guesser/Coin Flip Guesser/Program.cs
@@ -0,0 +1,9 @@
+﻿WriteLine("-----------------");
+WriteLine("Coin Flip Guesser");
+WriteLine("-----------------\n");
+//Write("Input a character between a and c: ");
+//char userPrompt = ReadKey().KeyChar;
+//WriteLine();
+Random randomize=new Random();
+char userPrompt = (char)randomize.Next('a', 'c');
+WriteLine($"With the character ({userPrompt}), the result of the flip is {(userPrompt=='a'?true:false)}");
---
+++ README.md.txt
@@ -0,0 +1,3 @@
+#Coin flip guesser using conditional operators
+
+The challange was to use conditional operator to simulate a coin flip by generating characters randomly from a given range and determining the value of the coin's face
