fb8a6e940cedcc34768265057d5a978bcbecea2f
Bwana Paul <p.ndungu.kara@gmail.com>
2023-12-08 07:54:15 +0300

    replaced d with t

--- Coin Flip Guesser/Coin Flip Guesser/Program.cs
+++ Coin Flip Guesser/Coin Flip Guesser/Program.cs
@@ -1,9 +1,9 @@
 ﻿WriteLine("-----------------");
 WriteLine("Coin Flip Guesser");
 WriteLine("-----------------\n");
-//Write("Input a character between h and d: ");
+//Write("Input a character between h and t: ");
 //char userPrompt = ReadKey().KeyChar;
 //WriteLine();
 Random randomize=new Random();
-char userPrompt = (char)randomize.Next('h', 'd');
+char userPrompt = (char)randomize.Next('h', 't');
 WriteLine($"With the character ({userPrompt}), the result of the flip is {(userPrompt=='h'?"heads":"tails")}");
