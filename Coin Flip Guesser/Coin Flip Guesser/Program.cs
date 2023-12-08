42335c1b4b8a12055ce0c745cff98cfb7d719a80
Bwana Paul <p.ndungu.kara@gmail.com>
2023-12-08 07:50:34 +0300

    changed a and b to h and t and changed true or false to heads tor tails

--- Coin Flip Guesser/Coin Flip Guesser/Program.cs
+++ Coin Flip Guesser/Coin Flip Guesser/Program.cs
@@ -1,9 +1,9 @@
 ﻿WriteLine("-----------------");
 WriteLine("Coin Flip Guesser");
 WriteLine("-----------------\n");
-//Write("Input a character between a and c: ");
+//Write("Input a character between h and d: ");
 //char userPrompt = ReadKey().KeyChar;
 //WriteLine();
 Random randomize=new Random();
-char userPrompt = (char)randomize.Next('a', 'c');
-WriteLine($"With the character ({userPrompt}), the result of the flip is {(userPrompt=='a'?true:false)}");
+char userPrompt = (char)randomize.Next('h', 'd');
+WriteLine($"With the character ({userPrompt}), the result of the flip is {(userPrompt=='h'?"heads":"tails")}");
